e77664e7f0e692946a031ae92cddb938726e58e0
Tuan Lai <laituan245@kaist.ac.kr>
2016-12-18 17:29:36 +0900

    Analyze whether males curse more or females curse more

--- analysis.py
+++ analysis.py
@@ -1,6 +1,15 @@
 import re
+import requests
+import urllib
+import json
 
 DEFAULT_DELIMITER = '+++$+++'
+DEFAULT_URL = 'http://localhost:3000/'
+PROFANITY_DETECTION_ROUTE = 'profanity-detection'
+
+def is_profane(text):
+    r = requests.get(DEFAULT_URL + PROFANITY_DETECTION_ROUTE + '?' + urllib.urlencode({'q' : text}))
+    return json.loads(r.text)['profane']
 
 characters = {}
 f = open('data/movie_characters_metadata.txt')
@@ -24,8 +33,27 @@ for line in f:
     utterance_id = arr[0]
     utterance_character_id = arr[1]
     utterance_text = arr[-1]
-    utterances[utterance_id] = {'character_id': utterance_character_id, 'text': utterance_text.lower()}
+    utterances[utterance_id] = {'character_id': utterance_character_id,
+                                'character_gender': characters[utterance_character_id]['gender'],
+                                'is_profane': is_profane(utterance_text)}
 assert(utterances['L538433']['character_id'] == 'u3014')
-assert(utterances['L538433']['text'] == "When whoever it is makes their move, you won't be here to ask if he's the one.".lower())
 assert(len(utterances.keys()) == 304713)
 f.close()
+
+nb_female_lines = 0
+nb_male_lines = 0
+nb_female_profane_lines = 0
+nb_male_profane_lines = 0
+for utterance_id in utterances.keys():
+    cur_utterance = utterances[utterance_id]
+    spoken_by_male = (cur_utterance['character_gender'] == 'm')
+    spoken_by_female = (cur_utterance['character_gender'] == 'f')
+    is_profane_line = cur_utterance['is_profane']
+    nb_male_lines += int(spoken_by_male)
+    nb_male_profane_lines += int(spoken_by_male and is_profane_line)
+    nb_female_lines += int(spoken_by_female)
+    nb_female_profane_lines += int(spoken_by_female and is_profane_line)
+print 'nb_female_lines = ' + str(nb_female_lines)
+print 'nb_female_profane_lines = ' + str(nb_female_profane_lines)
+print 'nb_male_lines = ' + str(nb_male_lines)
+print 'nb_male_profane_lines = ' + str(nb_male_profane_lines)
